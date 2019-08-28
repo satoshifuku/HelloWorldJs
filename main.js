21c0a2a3e7c5b4078d6489b5c1f3dbe8ac89f679
Unknown <morokataben.j@gmail.com>
2019-08-28 15:55:28 +0900

    Add a function to show the local time with text

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ console.log("Hello World!");
 function helloWorld(){
     document.writeln('Hello World!');
 }
+function showTime(){
+    var now = new Date();
+    document.getElementById("time").innerHTML = now.toLocaleTimeString();
+    // setInterval('time()',1000);    
+}
